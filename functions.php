3334380b6da7468f33743d7acae5a412b9e78ddd
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-13 11:22:13 +0800

    css

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
-<?php 
-	 add_action( 'wp_enqueue_scripts', 'priotech_child_enqueue_styles' );
-	 function priotech_child_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
- ?>
+<?php
+add_action('wp_enqueue_scripts', 'priotech_child_enqueue_styles');
+function priotech_child_enqueue_styles()
+{
+	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
