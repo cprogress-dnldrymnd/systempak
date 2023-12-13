e21db63161325326cd42534bdb3135bae1eff8e9
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-13 13:20:07 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -10,7 +10,19 @@ remove_action('woocommerce_single_product_summary', 'woocommerce_template_single
  * @testedwith    WooCommerce 7
  * @donate $9     https://businessbloomer.com/bloomer-armada/
  */
- 
- remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
- add_action( 'woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 60 );
- 
+
+remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
+
+
+function woocommerce_js()
+{
+?>
+    <script>
+        jQuery(document).ready(function() {
+            jQuery('#accordion-container').appendTo('.summary .left');
+        });
+    </script>
+<?php
+}
+
+add_action('wp_footer', 'woocommerce_js');
