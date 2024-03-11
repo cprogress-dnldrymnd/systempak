6b981d8046cb33c87560c67aa040c7278d94985b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-11 22:54:49 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -215,7 +215,7 @@ function searchfilter($query)
             array(
                 array(
                     'key' => '_sku',
-                    'value' => 'XSDS323'
+                    'value' => 'XSDS323',
                     'compare' => 'LIKE'
                 )
             )
