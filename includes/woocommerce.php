a55c6c94f60ce65159f638a6e95fc43162b0ae03
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-11 22:56:35 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -210,6 +210,16 @@ function searchfilter($query)
 {
     if ($query->is_search && !is_admin()) {
         $query->set('post_type',array('product'));
+        $query->set(
+            'meta_query',
+            array(
+                array(
+                    'key' => '_sku',
+                    'value' => '4067',
+                    'compare' => 'LIKE'
+                )
+            )
+        );
     }
 
     return $query;
