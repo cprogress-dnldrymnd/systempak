3d1cb960904b0055422261d48c6f421bcf35c74a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-12 01:59:05 +0800

    Update search.php

--- search.php
+++ search.php
@@ -1,8 +1,33 @@
 <?php get_header() ?>
 <?php
+$args = array(
+    'post_type' => 'product',
+    'meta_query' => array(
+        array(
+            'key' => '_sku',
+            'value' => $_GET['s'],
+            'compare' => 'LIKE',
+        ),
+    ),
+);
+$query = new WP_Query($args);
 
 ?>
 
+<div class="search">
+    <ul>
+        <?php if ($query->have_posts()) { ?>
+            <?php while ($query->have_posts()) { ?>
+                <?php $query->the_post(); ?>
+                <li>
+                    <?php the_title() ?>
+                </li>
+            <?php  } ?>
+        <?php  } else { ?>
+            No Result Found
+        <?php  } ?>
+    </ul>
+</div>
 <div class="search">
     <ul>
         <?php if (have_posts()) { ?>
@@ -10,7 +35,6 @@
                 <?php the_post(); ?>
                 <li>
                     <?php the_title() ?>
-
                 </li>
             <?php  } ?>
         <?php  } else { ?>
