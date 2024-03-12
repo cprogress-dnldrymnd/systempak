125bf7c84a523b10e55b62fb74e8efbc63aed6e0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-13 02:11:29 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -43,7 +43,10 @@ function search_ajax()
 
     $the_query = new WP_Query($args);
 
-
+    echo '<pre>';
+    var_dump($args);
+    echo '</pre>';
+    
     $count = $the_query->found_posts;
     echo hide_load_more($count, $offset, $posts_per_page);
 
