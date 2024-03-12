294e25768edec908104feeba7ea248aba116a0f2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-13 01:07:49 +0800

    css

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -61,6 +61,10 @@ function ajax($offset, $event_type = 'html') {
 
     var $s = jQuery('#search-input').val();
 
+    var $post_type = jQuery('input[name="post_type"]').val();
+
+    console.log($post_type);
+
     $loading = jQuery('<div class="loading-results"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/></svg></div>');
 
     $archive_section.addClass('loading-post');
@@ -106,7 +110,8 @@ function ajax($offset, $event_type = 'html') {
             s: $s,
 
             offset: $offset_val,
-
+            
+            post_type: $post_type,
 
         },
 
