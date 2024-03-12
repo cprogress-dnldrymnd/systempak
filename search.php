07fa95c2c18598b0ebafe5f35a08e57290851017
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-13 00:49:57 +0800

    Update search.php

--- search.php
+++ search.php
@@ -5,8 +5,9 @@
     <form>
         <div class="search-by">
             <div class="search-by-wrapper">
+                <input type="checkbox" id="searchby_sku">
                 <label for="searchby_sku">
-                    <input type="checkbox" id="searchby_sku"> Search by SKU
+                    Search by SKU
                 </label>
             </div>
         </div>
