85ff70dcc85e5253f61945aa46ab17fb57576877
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-13 01:54:14 +0800

    css

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -105,3 +105,50 @@ function breadcrumbs()
 }
 
 add_shortcode('breadcrumbs', 'breadcrumbs');
+
+
+function search()
+{
+    ob_start();
+?>
+    <div class="search-section">
+        <form>
+            <div class="search-by">
+
+                <div class="search-by-wrapper">
+                    <input type="radio" id="searchby_product" name="post_type" value="product" checked>
+                    <label for="searchby_product">
+                        Product
+                    </label>
+                </div>
+
+                <div class="search-by-wrapper">
+                    <input type="radio" id="searchby_blog" name="post_type" value="post">
+                    <label for="searchby_blog">
+                        Blog
+                    </label>
+                </div>
+                <div class="search-by-wrapper">
+                    <input type="radio" id="searchby_page" name="post_type" value="page">
+                    <label for="searchby_page">
+                        Page
+                    </label>
+                </div>
+            </div>
+            <input type="text" name="s" value="<?= isset($_GET['s']) ? $_GET['s'] : '' ?>" placeholder="Please type what you are looking for.." id="search-input">
+        </form>
+
+        <div id="results" page="1">
+            <div class="results-holder">
+
+            </div>
+            <div id="loadmore-holder" class="d-none">
+                <button id="load-more"><span>Load More</span></button>
+            </div>
+        </div>
+    </div>
+<?php
+    return ob_get_clean();
+}
+
+add_shortcode('search', 'search');
--- search.php
+++ search.php
@@ -1,42 +1,6 @@
 <?php get_header() ?>
 
-
-<div class="search-section">
-    <form>
-        <div class="search-by">
-
-            <div class="search-by-wrapper">
-                <input type="radio" id="searchby_product" name="post_type" value="product" checked>
-                <label for="searchby_product">
-                    Product
-                </label>
-            </div>
-
-            <div class="search-by-wrapper">
-                <input type="radio" id="searchby_blog" name="post_type" value="post">
-                <label for="searchby_blog">
-                    Blog
-                </label>
-            </div>
-            <div class="search-by-wrapper">
-                <input type="radio" id="searchby_page" name="post_type" value="page">
-                <label for="searchby_page">
-                    Page
-                </label>
-            </div>
-        </div>
-        <input type="text" name="s" value="<?= isset($_GET['s']) ? $_GET['s'] : '' ?>" placeholder="Please type what you are looking for.." id="search-input">
-    </form>
-
-    <div id="results" page="1">
-        <div class="results-holder">
-
-        </div>
-        <div id="loadmore-holder" class="d-none">
-            <button id="load-more"><span>Load More</span></button>
-        </div>
-    </div>
-</div>
+<?= do_shortcode('[search]') ?>
 
 <?php get_footer() ?>
 
