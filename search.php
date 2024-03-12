6a71b56f7b65361ce49a7a6b96d287c0e497706c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-13 00:56:44 +0800

    css

--- search.php
+++ search.php
@@ -10,6 +10,24 @@
                     Search by SKU
                 </label>
             </div>
+            <div class="search-by-wrapper">
+                <input type="checkbox" id="searchby_product">
+                <label for="searchby_product">
+                    Products
+                </label>
+            </div>
+            <div class="search-by-wrapper">
+                <input type="checkbox" id="searchby_blog">
+                <label for="searchby_blog">
+                    Blog
+                </label>
+            </div>
+            <div class="search-by-wrapper">
+                <input type="checkbox" id="searchby_page">
+                <label for="searchby_page">
+                    Blog
+                </label>
+            </div>
         </div>
         <input type="text" name="s" value="<?= isset($_GET['s']) ? $_GET['s'] : '' ?>" placeholder="Search by product name or product sku" id="search-input">
     </form>
