0c808432833737c92ebffbb4fc1ae69de5f4053d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-14 22:05:43 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -239,7 +239,7 @@ function bbloomer_single_order_meta_box()
 ?>
     <div>
         <label for="#Quantities">Quantities</label>
-        <?php wp_editor('test', 'mysecondeditor'); ?>
+        <?php wp_editor('test', 'quantities'); ?>
     </div>
 <?php
 }
