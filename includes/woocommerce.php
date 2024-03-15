87aa4c9db7798b1f4ac8e268833cb2f5cf8c1d58
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-15 20:49:37 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -216,7 +216,64 @@ function _products()
 }
 
 add_shortcode('_products', '_products');*/
+function rudr_upload_file_by_url($image_url)
+{
 
+    // it allows us to use download_url() and wp_handle_sideload() functions
+    require_once(ABSPATH . 'wp-admin/includes/file.php');
+
+    // download to temp dir
+    $temp_file = download_url($image_url);
+
+    if (is_wp_error($temp_file)) {
+        return false;
+    }
+
+    // move the temp file into the uploads directory
+    $file = array(
+        'name'     => basename($image_url),
+        'type'     => mime_content_type($temp_file),
+        'tmp_name' => $temp_file,
+        'size'     => filesize($temp_file),
+    );
+    $sideload = wp_handle_sideload(
+        $file,
+        array(
+            'test_form'   => false // no needs to check 'action' parameter
+        )
+    );
+
+    if (!empty($sideload['error'])) {
+        // you may return error message if you want
+        return false;
+    }
+
+    // it is time to add our uploaded image into WordPress media library
+    $attachment_id = wp_insert_attachment(
+        array(
+            'guid'           => $sideload['url'],
+            'post_mime_type' => $sideload['type'],
+            'post_title'     => basename($sideload['file']),
+            'post_content'   => '',
+            'post_status'    => 'inherit',
+        ),
+        $sideload['file']
+    );
+
+    if (is_wp_error($attachment_id) || !$attachment_id) {
+        return false;
+    }
+
+    // update medatata, regenerate image sizes
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+    wp_update_attachment_metadata(
+        $attachment_id,
+        wp_generate_attachment_metadata($attachment_id, $sideload['file'])
+    );
+
+    return $attachment_id;
+}
 function _products()
 {
     ob_start();
@@ -230,8 +287,9 @@ function _products()
     $query = new WP_Query($args);
     echo "<pre>";
     var_dump(carbon_get_post_meta(8017, 'tech_sheets'));
-    carbon_set_post_meta( 8786, 'tech_sheets[0]/tech_sheet_heading', 'Hello World!' );
-    carbon_set_post_meta( 8786, 'tech_sheets[0]/tech_sheet_file', 10199 );
+    carbon_set_post_meta(8786, 'tech_sheets[0]/tech_sheet_heading', 'Hello World!');
+    carbon_set_post_meta(8786, 'tech_sheets[0]/tech_sheet_file', 10199);
+    rudr_upload_file_by_url('https://systempak.net/wp-content/uploads/2016/11/TDS-2108-2000ML.pdf');
     echo "</pre>";
 
     echo '<ul>';
