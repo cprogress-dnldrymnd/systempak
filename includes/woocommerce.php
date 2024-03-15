aa17a37acd3aafd6c33cf03b25a8decf54e35fcf
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-15 20:58:41 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -330,14 +330,14 @@ function _products()
             }
         }
         if ($custom_tab_content2 && $custom_tab_title2 == 'Tech Sheet' && count(extract_url($custom_tab_content2)) == 1) {
-            $pdf_url = str_replace("spnew.theprogressteam.com", "https://systempak.net", extract_url($custom_tab_content2)[0]);
+            $pdf_url = str_replace("spnew.theprogressteam.com", "systempak.net", extract_url($custom_tab_content2)[0]);
             echo $pdf_url;
             if (_url_is_valid($pdf_url)) {
                 _file_upload($pdf_url, get_the_title());
             }
         }
         if ($custom_tab_content3 && $custom_tab_title3 == 'Tech Sheet' && count(extract_url($custom_tab_content3)) == 1) {
-            $pdf_url = str_replace("spnew.theprogressteam.com", "https://systempak.net", extract_url($custom_tab_content3)[0]);
+            $pdf_url = str_replace("spnew.theprogressteam.com", "systempak.net", extract_url($custom_tab_content3)[0]);
             echo $pdf_url;
             if (_url_is_valid($pdf_url)) {
                 _file_upload($pdf_url, get_the_title());
