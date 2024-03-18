bdd9d1a4d8cd3148729479c47eac300172729c72
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-18 09:38:57 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -602,7 +602,7 @@ function tech_sheet_content()
             echo "<strong>$tech_sheet_heading</strong>";
         }
         if ($tech_sheet_file) {
-            echo '<a href="'.wp_get_attachment_url($tech_sheet_file).'">Download <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+            echo '<a class="download-tech-sheet" target="_blank" href="'.wp_get_attachment_url($tech_sheet_file).'">Download <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
             <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
             <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
           </svg></a>';
