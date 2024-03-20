00a69874f6d15b623f1ceda8707cff9c443436e7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 19:08:05 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -124,10 +124,7 @@ function action_woocommerce_before_add_to_cart_form()
             </div>
         </div>
     </div>
-    <div class="select-variation">
-        <hr>
-        <p><strong>Select product variation</strong></p>
-    </div>
+ 
 <?php
 }
 
