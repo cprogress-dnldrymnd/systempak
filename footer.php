d29610c66aa8b916b9a2dd48398f25544412d383
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-25 16:53:41 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -35,7 +35,7 @@ if (current_user_can('administrator')) {
                 </div>
                 <div class="modal-body">
                     <div id="modal-result">
-                        <?= do_shortcode('[add_to_cart id="10755"]') ?>
+                        <?= do_shortcode('[product_page id="10755"]') ?>
                     </div>
                 </div>
                 <div class="modal-footer">
