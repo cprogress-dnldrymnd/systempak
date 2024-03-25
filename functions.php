7b8110f2e26921c16a35f756f596a1dfdbc75792
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-25 15:42:33 +0800

    css

--- functions.php
+++ functions.php
@@ -12,6 +12,13 @@ function priotech_child_enqueue_styles()
 		wp_enqueue_style('systempak-swiper', vendor_dir . 'swiper/swiper-bundle.min.css');
 		wp_enqueue_script('systempak-swiper', vendor_dir . 'swiper/swiper-bundle.min.js');
 	}
+
+	if (is_page(8978)) {
+		wp_enqueue_style('systempak-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
+		wp_enqueue_script('systempak-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js');
+		wp_enqueue_script('systempak-checkout', assets_dir . '/stylesheets/checkout/checkout.css');
+	}
+
 	wp_enqueue_script('systempak-main', assets_dir . 'javascripts/main.js', array('jquery'), 2.4);
 }
 
