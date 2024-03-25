bc37a72788c69360d4d52f512392f594faee730f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-25 15:57:02 +0800

    css

---
+++ woocommerce/checkout/review-order.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * Review order table
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/review-order.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 5.2.0
+ */
+
+defined('ABSPATH') || exit;
+?>
+
+<table class="shop_table woocommerce-checkout-review-order-table">
+
+	<tbody>
+		<?php
+		do_action('woocommerce_review_order_before_cart_contents');
+
+		foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
+			$_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
+
+			if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters('woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key)) {
+		?>
+				<tr class="<?php echo esc_attr(apply_filters('woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key)); ?>">
+					<td class="product-name">
+						<div class="d-flex align-items-center">
+							<div class="product-image">
+								<?= $_product->get_image() ?>
+							</div>
+							<div>
+								<div class="name mb-4">
+									<?php echo wp_kses_post(apply_filters('woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key)) . '&nbsp;'; ?>
+								</div>
+								<?php echo apply_filters('woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf('&times;&nbsp;%s', $cart_item['quantity']) . '</strong>', $cart_item, $cart_item_key); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+								?>
+								<?php echo wc_get_formatted_cart_item_data($cart_item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+								?>
+							</div>
+						</div>
+
+					</td>
+					<td class="product-total">
+						<div class="d-flex flex-column justify-content-between align-items-end">
+							<?php
+							$product_id = apply_filters('woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key);
+							?>
+							<a class="remove-item mb-3" target="shipping_method_qty_<?= $product_id ?>">
+								<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none">
+									<path d="M9 1L1 9M9 9L1 1" stroke="#16110E" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+								</svg>
+							</a>
+							<?php echo apply_filters('woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal($_product, $cart_item['quantity']), $cart_item, $cart_item_key); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+							?>
+						</div>
+					</td>
+				</tr>
+		<?php
+			}
+		}
+		do_action('woocommerce_review_order_after_cart_contents');
+
+		?>
+		<tr class="coupon">
+			<td colspan="2" class="td-coupon">
+				<div class="checkout_coupon_custom">
+					<p class="form-row form-row-first">
+						<input type="text" name="coupon_code_custom" class="input-text" placeholder="Coupon code" id="coupon_code_custom">
+					</p>
+					<p class="form-row form-row-last">
+						<a class="button apply_coupon_custom">Apply coupon</a>
+					</p>
+				</div>
+				<div class="coupon-message"></div>
+				<div class="notice">
+					<strong>Important:</strong> Before purchasing if you’re intending to use hardware for commercial international use, please enquire directly to explore export regulations and guidelines from our expert sales team, purchases that breach manufacturers international sales policies are not eligible for refund.
+				</div>
+			</td>
+		</tr>
+
+	</tbody>
+	<tfoot>
+
+		<tr class="cart-subtotal">
+			<th><?php esc_html_e('Subtotal', 'woocommerce'); ?></th>
+			<td><?php wc_cart_totals_subtotal_html(); ?></td>
+		</tr>
+
+		<?php foreach (WC()->cart->get_coupons() as $code => $coupon) : ?>
+			<tr class="cart-discount coupon-<?php echo esc_attr(sanitize_title($code)); ?>">
+				<th><?php wc_cart_totals_coupon_label($coupon); ?></th>
+				<td><?php wc_cart_totals_coupon_html($coupon); ?></td>
+			</tr>
+		<?php endforeach; ?>
+
+
+
+		<?php foreach (WC()->cart->get_fees() as $fee) : ?>
+			<tr class="fee">
+				<th><?php echo esc_html($fee->name); ?></th>
+				<td><?php wc_cart_totals_fee_html($fee); ?></td>
+			</tr>
+		<?php endforeach; ?>
+
+		<?php if (wc_tax_enabled() && !WC()->cart->display_prices_including_tax()) : ?>
+			<?php if ('itemized' === get_option('woocommerce_tax_total_display')) : ?>
+				<?php foreach (WC()->cart->get_tax_totals() as $code => $tax) : // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited 
+				?>
+					<tr class="tax-rate tax-rate-<?php echo esc_attr(sanitize_title($code)); ?>">
+						<th><?php echo esc_html($tax->label); ?></th>
+						<td><?php echo wp_kses_post($tax->formatted_amount); ?></td>
+					</tr>
+				<?php endforeach; ?>
+			<?php else : ?>
+				<tr class="tax-total">
+					<th><?php echo esc_html(WC()->countries->tax_or_vat()); ?></th>
+					<td><?php wc_cart_totals_taxes_total_html(); ?></td>
+				</tr>
+			<?php endif; ?>
+		<?php endif; ?>
+
+		<?php do_action('woocommerce_review_order_before_order_total'); ?>
+
+		<tr class="order-total">
+			<th><?php esc_html_e('Total', 'woocommerce'); ?></th>
+			<td><?php wc_cart_totals_order_total_html(); ?></td>
+		</tr>
+
+		<?php do_action('woocommerce_review_order_after_order_total'); ?>
+
+	</tfoot>
+</table>
