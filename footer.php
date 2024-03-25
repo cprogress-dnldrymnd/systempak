911b1ebb9ac3ebfb02ab65dcb78e94ea132b1c23
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-25 18:35:58 +0800

    css

--- footer.php
+++ footer.php
@@ -35,7 +35,7 @@ if (current_user_can('administrator')) {
                 </div>
                 <div class="modal-body">
                     <?php
-                    echo do_shortcode('[add_to_cart_form id="9150"]');
+                    echo do_shortcode('[add_to_cart_form id="10755"]');
                     ?>
                     <div id="modal-result">
 
