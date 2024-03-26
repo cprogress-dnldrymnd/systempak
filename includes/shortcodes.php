3419ba16cdf80e7b61658e9136102eab36e61ed4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-26 16:27:13 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -158,7 +158,7 @@ function search_products()
 {
     ob_start();
 ?>
-    <div class="search-section search-section-products mb-5">
+    <div class="search-section search-section-products">
         <form>
             <input class="w-100 mb-5" type="text" name="s" value="<?= isset($_GET['s']) ? $_GET['s'] : '' ?>" placeholder="Search products" id="search-input-product">
         </form>
