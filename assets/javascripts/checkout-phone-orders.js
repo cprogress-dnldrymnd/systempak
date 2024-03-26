4db7ae3076936f11660b1428886aa2d3d7a0bd1b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-26 16:58:05 +0800

    css

--- assets/javascripts/checkout-phone-orders.js
+++ assets/javascripts/checkout-phone-orders.js
@@ -261,10 +261,18 @@ function select_products() {
     jQuery(document).on('click', '.product-add-to-basket', function () {
         $product_id = jQuery(this).attr('product-id');
         $post_item = jQuery('.post-' + $product_id);
-        $post_item.find('.product-add-to-basket').text('Remove to basket').addClass('product-remove-to-basket').removeClass('.product-add-to-basket');
+        $post_item.find('.product-add-to-basket').text('Remove to basket').addClass('product-remove-to-basket').removeClass('product-add-to-basket');
         $post_item.appendTo('.selected-products .post-item-holder');
-        //ajax_select_product($product_id, jQuery(this));
     });
+
+    jQuery(document).on('click', '.product-remove-to-basket', function () {
+        $product_id = jQuery(this).attr('product-id');
+        $post_item = jQuery('.post-' + $product_id);
+        $post_item.find('.product-remove-to-basket').text('Add to baskset').addClass('product-add-to-basket').removeClass('product-remove-to-basket');
+        $post_item.appendTo('.select-products .results-holder .post-item-holder');
+    });
+
+
 }
 
 
