8dfae8bb5e7247e59fd5e2c6ac213023031fcf4b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-26 19:54:26 +0800

    cs

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -230,7 +230,65 @@ add_shortcode('add_to_cart_form', 'add_to_cart_form_shortcode');
 
 function woocommerce_checkout_custom()
 {
-    return do_shortcode('[woocommerce_checkout]');
+    ob_start();
+?>
+    <?php if (current_user_can('administrator')) { ?>
+        <section class="select-user py-5">
+            <div class="container-fluid">
+                <div class="inner">
+                    <h3>Please select a user first before creating order</h3>
+                    <div class="row gy-2">
+                        <?php foreach (get_users() as $user) { ?>
+                            <?php
+                            $link = user_switching::maybe_switch_url($user);
+                            ?>
+                            <div class="col-12">
+                                <div class="d-flex justify-content-between">
+                                    <strong><?= $user->display_name ?></strong>
+                                    <a class="button" href="<?= $link ?>&redirect_to=https://spnew.theprogressteam.com/phone-orders/">
+                                        Select Customer
+                                    </a>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <style>
+            .entry-content .woocommerce {
+                display: none !important;
+            }
+        </style>
+    <?php } else { ?>
+        <?php if (is_user_logged_in()) { ?>
+
+            <section class="logged-in-as mb-5">
+                <?php
+                $current_user = wp_get_current_user();
+                ?>
+                <div class="container-fluid">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="mb-0">Logged-in as <?= $current_user->user_email ?></h5>
+                        <h5 class="mb-0">
+                            <?php
+                            $old_user = user_switching::get_old_user();
+                            if ($old_user) {
+                                printf(
+                                    '<a href="%1$s">Switch back to %2$s</a>',
+                                    esc_url(user_switching::switch_back_url($old_user)) . '&redirect_to=https://spnew.theprogressteam.com/phone-orders/',
+                                    esc_html($old_user->display_name)
+                                );
+                            }
+                            ?>
+                        </h5>
+                    </div>
+                </div>
+            </section>
+        <?php } ?>
+    <?php } ?>
+<?php
+    return ob_get_clean();
 }
 
 add_shortcode('woocommerce_checkout_custom', 'woocommerce_checkout_custom');
