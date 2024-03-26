232b2ec4abea03908762b6fc64f8b702830eb6cd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-26 19:02:57 +0800

    Update page-phone-orders.php

--- templates/page-phone-orders.php
+++ templates/page-phone-orders.php
@@ -10,10 +10,14 @@
         <h3>Please select a user first before creating order</h3>
         <div class="row g-2">
             <?php foreach (get_users() as $user) { ?>
+
+                <?php
+                $link = user_switching::maybe_switch_url($user);
+                ?>
                 <div class="col-12">
                     <div class="d-flex justify-content-between">
                         <strong><?= $user->display_name ?></strong>
-                        <a class="button" href="<?= wp_login_url() ?>?action=switch_to_user&user_id=<?= $user->ID ?>&nr=1&_wpnonce=<?= wp_create_nonce() ?>">
+                        <a class="button" href="<?= $link ?>">
                             Select Customer
                         </a>
                     </div>
