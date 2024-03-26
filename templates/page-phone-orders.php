a95d30126fa08828d727363e6bc475c84374a260
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-26 18:51:41 +0800

    Update page-phone-orders.php

--- templates/page-phone-orders.php
+++ templates/page-phone-orders.php
@@ -8,10 +8,16 @@
 <?php if (current_user_can('administrator')) { ?>
     <section class="select-user">
         <h3>Please select a user first before creating order</h3>
-        <div class="row g-4">
+        <div class="row g-5">
             <?php foreach (get_users() as $user) { ?>
                 <div class="col-12">
-                    <?= $user->display_name ?>
+                    <div class="d-flex justify-content-between">
+                        <strong><?= $user->display_name ?></strong>
+                        <a class="button" href="<?= wp_login_url() ?>">
+                            Select Customer
+                        </a>
+                    </div>
+                    <hr>
                 </div>
             <?php } ?>
         </div>
