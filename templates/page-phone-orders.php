7eed11cbd66d98219d7fe65877dbf8d06bcfbc5d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-26 19:29:49 +0800

    Update page-phone-orders.php

--- templates/page-phone-orders.php
+++ templates/page-phone-orders.php
@@ -9,7 +9,7 @@
     <section class="select-user py-5" style="max-width: 800px">
         <div class="container-fluid">
             <h3>Please select a user first before creating order</h3>
-            <div class="row g-2">
+            <div class="row">
                 <?php foreach (get_users() as $user) { ?>
                     <?php
                     $link = user_switching::maybe_switch_url($user);
