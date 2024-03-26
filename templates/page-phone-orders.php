259455a4f7061d29b4125e756533e6c576eaba46
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-26 19:28:37 +0800

    Update page-phone-orders.php

--- templates/page-phone-orders.php
+++ templates/page-phone-orders.php
@@ -7,21 +7,23 @@
 
 <?php if (current_user_can('administrator')) { ?>
     <section class="select-user" style="max-width: 500px">
-        <h3>Please select a user first before creating order</h3>
-        <div class="row g-2">
-            <?php foreach (get_users() as $user) { ?>
-                <?php
-                $link = user_switching::maybe_switch_url($user);
-                ?>
-                <div class="col-12">
-                    <div class="d-flex justify-content-between">
-                        <strong><?= $user->display_name ?></strong>
-                        <a class="button" href="<?= $link ?>&redirect_to=https://spnew.theprogressteam.com/phone-orders/">
-                            Select Customer
-                        </a>
+        <div class="container-fluid">
+            <h3>Please select a user first before creating order</h3>
+            <div class="row g-2">
+                <?php foreach (get_users() as $user) { ?>
+                    <?php
+                    $link = user_switching::maybe_switch_url($user);
+                    ?>
+                    <div class="col-12">
+                        <div class="d-flex justify-content-between">
+                            <strong><?= $user->display_name ?></strong>
+                            <a class="button" href="<?= $link ?>&redirect_to=https://spnew.theprogressteam.com/phone-orders/">
+                                Select Customer
+                            </a>
+                        </div>
                     </div>
-                </div>
-            <?php } ?>
+                <?php } ?>
+            </div>
         </div>
     </section>
 <?php } else { ?>
