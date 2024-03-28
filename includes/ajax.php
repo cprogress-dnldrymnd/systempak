6ae56874d831908d8af20e620c2389b36d11dabb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-28 16:13:22 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -338,6 +338,7 @@ function cart_update_qty_script() {
     <script>
     jQuery( 'div.woocommerce' ).on( 'click', '.apply_custom_shipping_cost', function () {
         var fee = jQuery('#custom_radio_field_field  input:radio').val();
+        console.log(fee);
         jQuery.ajax({
             type: 'POST',
             url: "/wp-admin/admin-ajax.php",
