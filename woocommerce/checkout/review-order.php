bbe3d6ffb5284a4c161759dd9f0cbd1b9cde11d1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-28 17:45:35 +0800

    Update review-order.php

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -59,6 +59,9 @@ defined('ABSPATH') || exit;
 								</svg>
 							</a>
 							<?php echo apply_filters('woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal($_product, $cart_item['quantity']), $cart_item, $cart_item_key); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+
+							echo WC()->cart->get_product_subtotal($_product, 1);
+
 							?>
 						</div>
 					</td>
