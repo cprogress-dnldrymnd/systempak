23fb8ce173b8e9e9bab7fc7e79a215bf52f5fa4b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-01 14:46:59 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -90,7 +90,7 @@ $old_user = user_switching::get_old_user();
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="addCustomProductLabel">Modal title</h5>
+                    <h5 class="modal-title" id="addCustomProductLabel">Add Custom Product</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
