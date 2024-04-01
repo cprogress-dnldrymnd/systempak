7f65c27961861be0bef434da3f64798809e65758
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-01 17:15:24 +0800

    Update checkout-phone-orders.php

--- includes/checkout-phone-orders.php
+++ includes/checkout-phone-orders.php
@@ -483,7 +483,7 @@ function action_delete_custom_products($order_id)
         $product_id = $item->get_product_id();
         $delete_product = get_post_meta($product_id, 'delete_product', true);
 
-        echo $item->get_name() . ' ';
+        echo get_the_title($product_id). ' ';
         echo $delete_product;
     }
 }
