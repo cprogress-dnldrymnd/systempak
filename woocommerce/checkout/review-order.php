fe182855310e2867aa8f2cc2fda58be72e5be672
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-01 16:58:40 +0800

    Update review-order.php

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -38,7 +38,7 @@ defined('ABSPATH') || exit;
 							</div>
 							<div>
 								<div class="name mb-4">
-									<?php echo wp_kses_post(apply_filters('woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key)) . '&nbsp;'; ?>
+									<span class="name-wrapper"><?php echo wp_kses_post(apply_filters('woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key)) . '&nbsp;'; ?></span>
 									<div><?php echo 'Price per unit: '.WC()->cart->get_product_subtotal($_product, 1); ?></div>
 								</div>
 								<?php echo apply_filters('woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf('&times;&nbsp;%s', $cart_item['quantity']) . '</strong>', $cart_item, $cart_item_key); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
