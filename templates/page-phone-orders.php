8c31c610d319ed5335a864670eca9efeaefc4673
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-04 23:16:31 +0800

    Update page-phone-orders.php

--- templates/page-phone-orders.php
+++ templates/page-phone-orders.php
@@ -15,32 +15,47 @@ $old_user = user_switching::get_old_user();
     $users = get_users($args);
 
     ?>
-    <section class="select-user py-5">
-        <div class="container-fluid">
-            <div class="inner">
-                <h3>Please select a user first before creating order</h3>
-                <div class="row gy-2">
-                    <?php foreach ($users as $user) { ?>
-                        <?php
-                        $link = user_switching::maybe_switch_url($user);
-                        ?>
-                        <div class="col-12">
-                            <div class="d-flex justify-content-between">
-                                <strong><?= $user->user_email ?></strong>
-                                <a class="button" href="<?= $link ?>&redirect_to=https://systempak.net/phone-orders/">
-                                    Select Customer
-                                </a>
 
-                            </div>
-                        </div>
-                    <?php } ?>
+    <section class="select-user-form py-5">
+        <div class="container">
+            <form action="">
+
+            </form>
+            <div id="results">
+                <div class="results-holder">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email Address</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($users as $user) { ?>
+
+                                <?php
+                                $link = user_switching::maybe_switch_url($user);
+                                ?>
+                                <tr>
+                                    <td><?= $user->display_name ?> </td>
+                                    <td><?= $user->user_email ?></td>
+                                    <td>
+                                        <a class="button" href="<?= $link ?>&redirect_to=https://systempak.net/phone-orders/">
+                                            Select Customer
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </section>
 <?php } else { ?>
     <?php if (is_user_logged_in() && $old_user) { ?>
-
         <section class="checkout-form">
             <div class="container-fluid">
                 <a href="<?= get_site_url() ?>"></a>
