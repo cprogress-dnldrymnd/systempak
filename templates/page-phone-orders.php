38ffdca4b5b0e329c034c68f9279096ec23de80c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-04 23:28:36 +0800

    Update page-phone-orders.php

--- templates/page-phone-orders.php
+++ templates/page-phone-orders.php
@@ -12,7 +12,6 @@ $old_user = user_switching::get_old_user();
     $args = array(
         'role' => array('customer'),
         'number' => 10,
-        'search' => 'Amanda'
     );
     $user_query = new WP_User_Query($args);
 
