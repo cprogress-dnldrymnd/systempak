3173a72a0588b71d17bbd89210971bf53ac0f6b4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-08 18:00:53 +0800

    Update checkout-phone-orders.php

--- includes/checkout-phone-orders.php
+++ includes/checkout-phone-orders.php
@@ -175,7 +175,7 @@ function add_custom_extra_fee($cart)
         $cart->fees_api()->set_fees($fees);
     }
 
-    WC()->session->set( 'sess_variable_name', null );
+    WC()->session->set( 'Custom Shipping Cost', null );
 }
 
 
