cb182290846a5749e7c5df3470a7a5b29cc21cf4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-09 05:00:11 +0800

    cs

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -78,20 +78,7 @@ defined('ABSPATH') || exit;
 		do_action('woocommerce_review_order_after_cart_contents');
 
 		?>
-		<tr class="coupon custom-forms">
-			<td colspan="2" class="td-coupon">
-				<div class="checkout_coupon_custom">
-					<p class="form-row form-row-first">
-						<input type="text" name="coupon_code_custom" class="input-text" placeholder="Coupon code" id="coupon_code_custom">
-					</p>
-					<p class="form-row form-row-last">
-						<a class="button apply_coupon_custom">Apply coupon</a>
-					</p>
-				</div>
-				<div class="coupon-message"></div>
-
-			</td>
-		</tr>
+	
 
 		<tr class="custom-shipping custom-forms" id="custom-shipping-cost">
 			<td colspan="2" class="td-coupon">
