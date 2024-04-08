622671cb75d7d6b5163ea1f5ebb9777450962a42
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-09 05:23:24 +0800

    Update checkout-phone-orders.php

--- includes/checkout-phone-orders.php
+++ includes/checkout-phone-orders.php
@@ -659,11 +659,12 @@ function disable_shipping_calc_on_cart($show_shipping)
 add_filter('woocommerce_cart_ready_to_calc_shipping', 'disable_shipping_calc_on_cart', 99);
 
 
-remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
-add_action( 'woocommerce_review_order_after_cart_contents', 'woocommerce_checkout_coupon_form_custom' );
-function woocommerce_checkout_coupon_form_custom() {
+remove_action('woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10);
+add_action('woocommerce_review_order_after_cart_contents', 'woocommerce_checkout_coupon_form');
+function woocommerce_checkout_coupon_form_custom()
+{
     echo '<tr class="coupon-form"><td colspan="2">';
-    
+
     wc_get_template(
         'checkout/form-coupon.php',
         array(
