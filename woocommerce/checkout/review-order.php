43c457fbb3a5be134f0e835f2d2472d1fe1ddd3e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-09 05:17:52 +0800

    cs

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -75,15 +75,10 @@ defined('ABSPATH') || exit;
 		<?php
 			}
 		}
-
+		
 
 		?>
 
-		<tr class="coupon custom-forms">
-			<td>
-				<?php do_action('custom_coupon_form'); ?>
-			</td>
-		</tr>
 		<tr class="coupon custom-forms">
 			<td colspan="2" class="td-coupon">
 				<div class="checkout_coupon_custom">
