11376044eaba46dd4f236d114cbac8e0b2829d85
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-10 22:54:58 +0800

    Update checkout-phone-orders.php

--- includes/checkout-phone-orders.php
+++ includes/checkout-phone-orders.php
@@ -491,7 +491,7 @@ function action_custom_checkout()
 
 
 
-        jQuery('div.woocommerce').on('click', '.apply_custom_shipping_cost', function() {
+        jQuery('#custom-shipping-cost').on('click', '.apply_custom_shipping_cost', function() {
             var custom_shipping_cost = jQuery('#custom-shipping-cost input[name="custom_shipping_cost"]').val();
             console.log(custom_shipping_cost);
             jQuery.ajax({
