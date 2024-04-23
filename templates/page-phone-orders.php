b8a3dd7d533d340639fc7759dec79ce682697b03
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-23 22:33:30 +0800

    Update page-phone-orders.php

--- templates/page-phone-orders.php
+++ templates/page-phone-orders.php
@@ -20,7 +20,19 @@ echo '<br><br>';
 
     <section class="select-user-form py-5">
         <div class="container">
-
+            <div id="addNewCustomer">
+                <form action="<?= get_permalink() ?>" method="POST">
+                    <div class="form-holder">
+                        <div class="row g-0 m-0">
+                            <div class="col">
+                                <input type="text" class="form-control" name="email" id="email" placeholder="Customer Email Address">
+                                <input type="text" class="form-control" name="username" id="username" placeholder="Customer Username">
+                                <input type="password" class="form-control" name="password" id="password" placeholder="Customer Password">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
             <div id="userSearchForm">
                 <div class="form-holder">
                     <h3>Customer Search Form</h3>
@@ -70,10 +82,12 @@ echo '<br><br>';
                     <div class="loading-style-1 d-none">
                         <svg class="adding-product" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                             <path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"></path>
-                        </svg>                                                                                      
+                        </svg>
                     </div>
                 </div>
             </div>
+
+
 
         </div>
     </section>
@@ -84,7 +98,7 @@ echo '<br><br>';
                 <a href="<?= get_site_url() ?>"></a>
                 <?= do_shortcode('[woocommerce_checkout]') ?>
             </div>
-        </section>                                                          
+        </section>
     <?php } ?>
 
 
