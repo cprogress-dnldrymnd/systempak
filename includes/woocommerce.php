60475f0db0143b35fe638e8cf60a1966cacefa74
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-03 00:06:49 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -855,12 +855,3 @@ function custom_email_order_items_args($args)
 remove_action('woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10);
 
 
-/**
- * Disable messages about the mobile apps in WooCommerce emails.
- * https://wordpress.org/support/topic/remove-process-your-orders-on-the-go-get-the-app/
- */
-function mtp_disable_mobile_messaging($mailer)
-{
-    remove_action('woocommerce_email_footer', array($mailer()->emails['WC_Email_New_Order'], 'mobile_messaging'), 9);
-}
-add_action('woocommerce_email', 'mtp_disable_mobile_messaging');
