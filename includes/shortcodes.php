809c741ae6707ced9f16e513ab6bba4fe7f9114e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-15 00:11:54 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -2,45 +2,47 @@
 function product_category_subcategory()
 {
     ob_start();
-    $current = get_queried_object();
-    $terms = get_terms(array(
-        'taxonomy' => 'product_cat',
-        'parent'   => $current->term_id
-    ));
+    if (current_user_can('administrator')) {
+        $current = get_queried_object();
+        $terms = get_terms(array(
+            'taxonomy' => 'product_cat',
+            'parent'   => $current->term_id
+        ));
 
 ?>
-    <div class="product-category-slider">
-        <div class="swiper mySwiper-ProductCategory">
-            <div class="swiper-wrapper">
-                <?php foreach ($terms as $term) { ?>
-                    <?php
-                    $thumbnail_id = get_term_meta($term->term_id, 'thumbnail_id', true);
-                    ?>
-                    <div class="swiper-slide">
-                        <a href="<?= get_term_link($term->term_id) ?>"></a>
-                        <div class="term-name">
-                            <h4><?= $term->name ?></h4>
+        <div class="product-category-slider">
+            <div class="swiper mySwiper-ProductCategory">
+                <div class="swiper-wrapper">
+                    <?php foreach ($terms as $term) { ?>
+                        <?php
+                        $thumbnail_id = get_term_meta($term->term_id, 'thumbnail_id', true);
+                        ?>
+                        <div class="swiper-slide">
+                            <a href="<?= get_term_link($term->term_id) ?>"></a>
+                            <div class="term-name">
+                                <h4><?= $term->name ?></h4>
+                            </div>
+                            <div class="product-counts">
+                                <span>4 Products</span>
+                            </div>
+                            <div class="image-box">
+                                <img src="<?= wp_get_attachment_url($thumbnail_id, 'large') ?>" alt="<?= $term->name ?>">
+                            </div>
+                            <div class="circle-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+                                </svg>
+                            </div>
                         </div>
-                        <div class="product-counts">
-                            <span>4 Products</span>
-                        </div>
-                        <div class="image-box">
-                            <img src="<?= wp_get_attachment_url($thumbnail_id, 'large') ?>" alt="<?= $term->name ?>">
-                        </div>
-                        <div class="circle-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
-                            </svg>
-                        </div>
-                    </div>
 
-                <?php } ?>
+                    <?php } ?>
+                </div>
+                <div class="swiper-pagination"></div>
             </div>
-            <div class="swiper-pagination"></div>
         </div>
-    </div>
-<?php
-    return ob_get_clean();
+    <?php
+        return ob_get_clean();
+    }
 }
 
 add_shortcode('product_category_subcategory', 'product_category_subcategory');
@@ -58,7 +60,7 @@ add_shortcode('term_name', 'term_name');
 
 function term_description_sc()
 {
-    return '<div class="term-description">'.wpautop(get_queried_object()->description).'<div class="term-desc-read-more"><button class="read-more-term-desc"> Read More </button></div></div>';
+    return '<div class="term-description">' . wpautop(get_queried_object()->description) . '<div class="term-desc-read-more"><button class="read-more-term-desc"> Read More </button></div></div>';
 }
 
 add_shortcode('term_description', 'term_description_sc');
@@ -110,7 +112,7 @@ add_shortcode('breadcrumbs', 'breadcrumbs');
 function search()
 {
     ob_start();
-?>
+    ?>
     <div class="search-section search-section-default">
         <form action="/" method="GET">
             <div class="search-by">
