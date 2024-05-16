ac22e8548b43d2f0b1c58f47bb1366d9839f3530
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-16 23:47:33 +0800

    email

--- woocommerce/emails/email-footer.php
+++ woocommerce/emails/email-footer.php
@@ -35,6 +35,59 @@ defined( 'ABSPATH' ) || exit;
 								<td align="center" valign="top">
 									<!-- Footer -->
 									<table border="0" cellpadding="10" cellspacing="0" width="100%" id="template_footer">
+									<tr>
+											<td style="padding-left: 38px; padding-right: 38px">
+												<table class="bank-details"  border="0" cellpadding="10" cellspacing="0" width="100%">
+													<tr>
+														<td colspan="2"  valign="top" style="font-size: 14px; color: #000">
+															<strong>BANK DETAILS</strong>
+														</td>
+													</tr>
+													<tr>
+														<td colspan="2" valign="top" style="font-size: 12px; border-bottom: 2px solid #F2F2F2; padding-bottom: 20px !important;color: #000">
+															Bank details are as follows should you have selected to pay by bank transfer.
+														</td>
+													</tr>
+													<tr>
+														<td class="fs-11"  style="width: 120px; color: #000">
+															<strong>BANK NAME:</strong>
+														</td>
+														<td class="fs-11" style="color: #000">
+															Natwest
+														</td>
+													</tr>
+													<tr>
+														<td class="fs-11" style="color: #000">
+															<strong>ACCOUNT NAME:</strong>
+														</td>
+														<td class="fs-11" style="color: #000">
+															Systempak LTD
+														</td>
+													</tr>
+													<tr>
+														<td class="fs-11" style="color: #000">
+															<strong>SORT CODE:</strong>
+														</td>
+														<td class="fs-11" style="color: #000">
+															60-21-36
+														</td>
+													</tr>
+													<tr>
+														<td class="fs-11" style="color: #000">
+															<strong>ACCOUNT NUMBER:</strong>
+														</td>
+														<td class="fs-11" style="color: #000">
+															5750 6620
+														</td>
+													</tr>
+													<tr>
+														<td colspan="2" valign="top" style="font-size: 12px; border-top: 2px solid #F2F2F2; padding-top: 20px !important; color: #000">
+															Please use your Order No. as the payment reference. Your order will not be shipped until the funds have cleared in our account.
+														</td>
+													</tr>
+												</table>
+											</td>
+										</tr>
 										<tr>
 											<td valign="top">
 												<table border="0" cellpadding="10" cellspacing="0" width="100%">
