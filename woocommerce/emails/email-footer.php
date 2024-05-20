a7d5b992024bad5752e2764bc36100bd3f7a1225
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-20 23:52:39 +0800

    Update email-footer.php

--- woocommerce/emails/email-footer.php
+++ woocommerce/emails/email-footer.php
@@ -35,7 +35,7 @@ defined( 'ABSPATH' ) || exit;
 								<td align="center" valign="top">
 									<!-- Footer -->
 									<table border="0" cellpadding="10" cellspacing="0" width="100%" id="template_footer">
-									<tr>
+										<tr>
 											<td style="padding-left: 38px; padding-right: 38px">
 												<table class="bank-details"  border="0" cellpadding="10" cellspacing="0" width="100%">
 													<tr>
