f894299a9fb3f14293899717ffc08b933f99bf6f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-18 16:56:33 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -10,7 +10,7 @@ function swiper_hero() {
     var swiper_hero = new Swiper('.swiper-hero', {
         loop: true,
         spaceBetween: 0,
-        speed: 2000,
+        speed: 1000,
         
         autoplay: {
             delay: 10000,
